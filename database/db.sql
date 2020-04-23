fbb70c87de6b61e820a68d20c0a2fa583911f385
Edu-Anton <eduardo1acc@gmail.com>
2020-04-23 06:48:17 -0500

    configuration completion

---
+++ database/db.sql
@@ -0,0 +1,19 @@
+-- creating the database
+CREATE DATABASE expressjs-cruddb;
+
+-- using de database
+use expressjs;
+
+-- creating a table
+CREATE TABLE customer (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone VARCHAR(15)
+);
+
+-- to show all tables
+SHOW TABLES;
+
+-- to describe the table
+describe customer
--- src/app.js
+++ src/app.js
@@ -1,7 +1,37 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection')
 const app = express();
+//importing routes
+const customerRoutes = require('./routes/customer');
 
-app.listen(3000, () => {
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs') // No es necesario requerir ejs, sólo descargarlo como paquete
+app.set('views', path.join(__dirname, 'views'));
+
+// middlewares
+app.use(morgan('dev'))
+app.use(myConnection(mysql, {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  port:3306,
+  database: 'expressjs-cruddb'
+}, 'single')) // single hace referencia a una configuración de myConnection
+
+// routes
+app.use('/', customerRoutes);
+
+// static files : img, fuente, css, código fuente, scripts js 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// server
+app.listen(app.get('port'), () => {
   console.log('Server on port 3000')
 });
+
+
 
